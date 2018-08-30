76937c6793523ccc577b7659d9674c73cd0d11f2
Silvia <silvia@itell.io>
2018-08-30 13:41:09 -0400

    desktop nav

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,10 @@ document.querySelector('#close').addEventListener('click', function () {
 document.querySelector('#open-menu').addEventListener('click', function () {
   document.querySelector('.primary-nav').classList.add('open');
 });
+
+document.querySelector('.nav-list').addEventListener('mouseover', function () {
+  document.querySelector('.sub-nav').classList.add('open');
+});
+document.querySelector('.nav-list').addEventListener('mouseout', function () {
+  document.querySelector('.sub-nav').classList.remove('open');
+});
